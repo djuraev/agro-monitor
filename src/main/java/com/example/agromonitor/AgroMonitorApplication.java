6405b4526da66ad21fa9342b573a7f156a6d4a04
djuraev <mamurdjuraev@gmail.com>
2021-07-17 18:10:16 +0900

    Initial commit

---
+++ src/main/java/com/example/agromonitor/AgroMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.agromonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/agromonitor/AgroMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agromonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
