df8e9697f30d35547bba58b607dce1a3b3a2bbe9
djuraev <mamurdjuraev@gmail.com>
2021-07-17 18:18:42 +0900

    Initial frame and config

--- src/main/java/uz/agromon/AgroMonitorApplication.java
+++ src/main/java/uz/agromon/AgroMonitorApplication.java
@@ -1,4 +1,4 @@
-package com.example.agromonitor;
+package uz.agromon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/uz/agromon/AgroMonitorApplicationTests.java
+++ src/test/java/uz/agromon/AgroMonitorApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.agromonitor;
+package uz.agromon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
